42559303a6101b36f8da44763f2c15a649dfc1e2
Cassio Zen <caantoni@microsoft.com>
2019-12-16 15:43:32 -0600

    Better webpack typescript output (closer to TSC)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,39 @@
-const NodemonPlugin = require("nodemon-webpack-plugin"); // eslint-disable-line
+/* eslint-disable */
+const NodemonPlugin = require("nodemon-webpack-plugin");
+const CleanTerminalPlugin = require("clean-terminal-webpack-plugin");
+const fs = require("fs");
+
+const files = {};
+function readFileLine(file, line) {
+  if (!files[file]) {
+    files[file] = fs.readFileSync(file, "utf-8").split("\n");
+  }
+  return files[file][line];
+}
+
+function customTSErrorFormatter(error, colors) {
+  const { code, severity, content, file, line, character, context } = error;
+
+  const severityMsg =
+    severity === "warning"
+      ? colors.bold.yellow("warning")
+      : colors.bold.red("error");
+  const source = readFileLine(file, line - 1);
+  const shortFile = file.substr(context.length);
+  const colon = colors.white(":");
+  const fileInfo = `${colors.cyan(shortFile)}${colon}${colors.yellow(
+    line
+  )}${colon}${colors.yellow(character)}`;
+  const errorToken =
+    " ".repeat(line.toString.length + character + 1) + colors.red("^");
+  return `${fileInfo} - ${severityMsg} ${colors.grey(
+    `TS${code}: ${colors.bold.white(content)}`
+  )}
+
+${colors.black.bgWhiteBright(line)} ${colors.white(source)}
+${errorToken}
+  `;
+}
 
 module.exports = {
   mode: "production",
@@ -16,11 +51,8 @@ module.exports = {
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
+        loader: "ts-loader",
+        options: { errorFormatter: customTSErrorFormatter }
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
@@ -34,7 +66,8 @@ module.exports = {
   plugins: [
     new NodemonPlugin({
       quiet: true
-    })
+    }),
+    new CleanTerminalPlugin()
   ],
   stats: {
     assets: false,
