c9389fcede33637b500affb8835273ea0db9ee73
Cassio Zen <caantoni@microsoft.com>
2019-12-16 12:38:00 -0600

    React exercises & solution

--- src/Exercise_5_React/exercise5.tsx
+++ src/Exercise_5_React/exercise5.tsx
@@ -1,30 +1,38 @@
+import React, { Component, ReactChild } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import PropTypes from "prop-types";
+
 // ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
 //   Exercise 5 – React
 // ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
 
 // Objectives:
 // • Understand how you can use TypeScript with React
-
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
+// • Replacing propTypes with type aliases
+// • Functional and class components
+// • React specific types
 
 export default () => {
   // ======== Exercise 5.0 ========
-  // Blah Blah
-  // Instructions:
-  // • Loren ipsum
+  // The purpose of this exercise is simply to illustrate a that functional Components
+  // have no different between JavaScript and TypeScript
+
   const Hello = () => <h1>Hello from a React Component!</h1>;
 
-  console.log(`Hello component: ${renderToStaticMarkup(<Hello />)}`);
+  console.log(
+    "[Exercise 5.0]",
+    `Hello component: ${renderToStaticMarkup(<Hello />)}`
+  );
 
   // ======== Exercise 5.1 ========
-  // Blah Blah
   // Instructions:
-  // • Loren ipsum
-  const Card = ({ title, subtitle, collapsed, text }) => {
-    const classes = collapsed ? "card open" : "card collapsed";
+  // • Replace the card PropTypes for a Type Alias
+  // • As a good practice, declare the type alias before the component
+
+  const Card = ({ title, subtitle, collapsed, theme, text }) => {
+    const collapsedClass = collapsed ? "open" : "collapsed";
     return (
-      <div className={classes}>
+      <div className={`card ${collapsedClass} ${theme}`}>
         <h1>title</h1>
         {subtitle && <h2>{subtitle}</h2>}
         <p>{text}</p>
@@ -36,10 +44,90 @@ export default () => {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
     collapsed: PropTypes.bool.isRequired,
+    theme: PropTypes.oneOf(["active", "default", "frozen"]),
     text: PropTypes.string.isRequired
   };
 
   console.log(
-    `Card component: ${renderToStaticMarkup(<Card title="Sample Card" />)}`
+    "[Exercise 5.1]",
+    `Card component: ${renderToStaticMarkup(
+      <Card title="Sample Card" theme="active" />
+    )}`
+  );
+
+  // ======== Exercise 5.2 ========
+  // Notice that in `package.json`, we have "@types/react" as a dev dependency
+  // This exposes type definitions for common React structures (such as `props.children`)
+  // Instructions:
+  // • Created a type alias for the props
+  // • Use `ReactChild` (imported in the beggining of this file) for `children`
+
+  const PictureFrame = ({ children, legend }) => (
+    <div className="picture-frame">
+      {children}
+      <p className="legend">{legend}</p>
+    </div>
+  );
+
+  console.log(
+    "[Exercise 5.2]",
+    `PictureFrame: ${renderToStaticMarkup(
+      <PictureFrame legend="Random Picture">
+        <img src="https://picsum.photos/200" role="presentation" />
+      </PictureFrame>
+    )}`
+  );
+
+  // ======== Exercise 5.3 ========
+  // Class components with no props and no state don't need any type annotation.
+  // have no different between JavaScript and TypeScript.
+  // Nothing to do on this exercise, just observe.
+
+  class Menu extends Component {
+    render() {
+      return (
+        <menu>
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+        </menu>
+      );
+    }
+  }
+
+  console.log(
+    "[Exercise 5.3]",
+    `Menu component: ${renderToStaticMarkup(<Menu />)}`
+  );
+
+  // ======== Exercise 5.4 ========
+  // Class components with props and state need have their types declared
+  // Instructions:
+  // • Declare the component State and Props types using the <> notation:
+  // <Props, State>
+
+  type Props = { initialCount?: number };
+  type State = { count: number };
+
+  class Counter extends Component {
+    state = {
+      count: this.props.initialCount || 0
+    };
+    render() {
+      return (
+        <>
+          <h1>{this.state.count}</h1>
+          <button
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            Increase
+          </button>
+        </>
+      );
+    }
+  }
+
+  console.log(
+    "[Exercise 5.4]",
+    `Counter component: ${renderToStaticMarkup(<Counter initialCount={10} />)}`
   );
 };
--- src/Exercise_5_React/solution5.tsx
+++ src/Exercise_5_React/solution5.tsx
@@ -1,38 +1,34 @@
+import React, { Component, ReactChild } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+
 // ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
-//   Exercise 5 – React
+//   Solution 5 – React
 // ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
 
 // Objectives:
 // • Understand how you can use TypeScript with React
-
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
+// • Replacing propTypes with type aliases
+// • Functional and class components
+// • React specific types
 
 export default () => {
-  // ======== Exercise 5.0 ========
-  // Blah Blah
+  // ======== Solution 5.1 ========
   // Instructions:
-  // • Loren ipsum
-  const Hello = () => <h1>Hello from a React Component!</h1>;
-
-  console.log(`Here's your component: ${renderToStaticMarkup(<Hello />)}`);
-
-  // ======== Exercise 5.1 ========
-  // Blah Blah
-  // Instructions:
-  // • Loren ipsum
+  // • Replace the card PropTypes for a Type Alias
+  // • As a good practice, declare the type alias before the component
 
   type CardProps = {
     title: string;
     subtitle?: string;
     collapsed: boolean;
+    theme: "active" | "default" | "frozen";
     text: string;
   };
 
-  const Card = ({ title, subtitle, collapsed, text }: CardProps) => {
-    const classes = collapsed ? "card open" : "card collapsed";
+  const Card = ({ title, subtitle, collapsed, theme, text }: CardProps) => {
+    const collapsedClass = collapsed ? "open" : "collapsed";
     return (
-      <div className={classes}>
+      <div className={`card ${collapsedClass} ${theme}`}>
         <h1>title</h1>
         {subtitle && <h2>{subtitle}</h2>}
         <p>{text}</p>
@@ -41,12 +37,74 @@ export default () => {
   };
 
   console.log(
+    "[Exercise 5.1]",
     `Card component: ${renderToStaticMarkup(
       <Card
         title="Sample Card"
         collapsed={false}
         text="This is a sample card"
+        theme="active"
       />
     )}`
+  );
+
+  // ======== Exercise 5.2 ========
+  // Notice that in `package.json`, we have "@types/react" as a dev dependency
+  // This exposes type definitions for common React structures (such as `props.children`)
+  // Instructions:
+  // • Created a type alias for the props
+  // • Use `ReactChild` (imported in the beggining of this file) for `children`
+
+  type PictureProps = {
+    children: ReactChild;
+    legend: string;
+  };
+
+  const PictureFrame = ({ children, legend }: PictureProps) => (
+    <div className="picture-frame">
+      {children}
+      <p className="legend">{legend}</p>
+    </div>
+  );
+
+  console.log(
+    "[Exercise 5.2]",
+    `PictureFrame: ${renderToStaticMarkup(
+      <PictureFrame legend="Random Picture">
+        <img src="https://picsum.photos/200" role="presentation" />
+      </PictureFrame>
+    )}`
+  );
+
+  // ======== Exercise 5.4 ========
+  // Class components with props and state need have their types declared
+  // Instructions:
+  // • Declare the component State and Props types using the <> notation:
+  // <Props, State>
+
+  type Props = { initialCount?: number };
+  type State = { count: number };
+
+  class Counter extends Component<Props, State> {
+    state = {
+      count: this.props.initialCount || 0
+    };
+    render() {
+      return (
+        <>
+          <h1>{this.state.count}</h1>
+          <button
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            Increase
+          </button>
+        </>
+      );
+    }
+  }
+
+  console.log(
+    "[Exercise 5.4]",
+    `Counter component: ${renderToStaticMarkup(<Counter initialCount={10} />)}`
   );
 };
