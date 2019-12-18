23ea9cb759240e359d7ed3ab46a4060053e999c5
Cassio Zen <caantoni@microsoft.com>
2019-12-18 12:17:12 -0600

    eslinting react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended"]
+  extends: ["plugin:react/recommended","plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
 };
